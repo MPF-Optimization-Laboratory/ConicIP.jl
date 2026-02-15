f037a885068605aa492f43fbbf1403120e0a68f2
Michael Friedlander <michael@friedlander.io>
2026-02-14 18:22:06 -0800

    docs: add Documenter.jl infrastructure, landing page, and CI workflow

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter
+using ConicIP
+
+DocMeta.setdocmeta!(ConicIP, :DocTestSetup, :(using ConicIP); recursive=true)
+
+makedocs(;
+    modules  = [ConicIP],
+    sitename = "ConicIP.jl",
+    authors  = "MPF Optimization Laboratory",
+    format   = Documenter.HTML(;
+        canonical = "https://MPF-Optimization-Laboratory.github.io/ConicIP.jl",
+        edit_link = "master",
+    ),
+    pages = [
+        "Home" => "index.md",
+    ],
+    warnonly = [:missing_docs],
+)
+
+deploydocs(;
+    repo      = "github.com/MPF-Optimization-Laboratory/ConicIP.jl",
+    devbranch = "master",
+    push_preview = true,
+)
